d9408f1431c21f13d65187b7ee5371a1c4ca23f4
jspencersharpe <jspencersharpe@gmail.com>
2015-03-05 11:48:49 -0600

    checkbox functional

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required, StringLength(60, MinimumLength=3)]
+        public string Title { get; set; }
+        [Display(Name = "ReleaseDate"),DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Range(1, 100), DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        [Required,StringLength(5)]
+        public string Rating { get; set; }
+         public bool Razzie { get; set; }
+    }
+
+    public class MovieDBContext : DbContext 
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
